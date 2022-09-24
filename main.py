7f28ee8ba471ce010b4905ed06a205d5fea78b9c
Spaceox <91277246+Spaceox@users.noreply.github.com>
2022-09-24 21:24:53 +0200

    Better song search handling

--- main.py
+++ main.py
@@ -13,6 +13,7 @@ PlaylistName = "ToSpotify"
 SongDir = "music"
 SuccessDir = "done"
 IgnoredDir = "ignored"
+mkt = "IT" # Select your Spotify market (or leave it blank)
 
 # But not these ones
 scope = 'playlist-read-private, playlist-modify-private'
@@ -31,7 +32,7 @@ def getPlaylistID(playlists: list) -> string:
     print(f"Created playlist {PlaylistName}")
     return playlist['id']
 
-def moveFile(dir: string, file: string, ignored: bool = False) -> None:
+def moveFile(dir: string, file: string, ignored: bool = False, recognized: bool = False) -> None:
     if ignored:
         os.rename(f"{dir}\{file}", f"{IgnoredDir}\{file}")
     else:
@@ -43,9 +44,13 @@ async def getTrackID(songFile: string) -> string:
     if len(out['matches']) != 0:
         serialized = Serialize.track(data=out["track"])
         print(f"Recognized song: {serialized.subtitle} - {serialized.title}")
-        songSearch = sp.search(q=f'artist: "{serialized.subtitle}" track: "{serialized.title}"', limit=1)
-        print(f"Song in Spotify: {songSearch['tracks']['items'][0]['artists'][0]['name']} - {songSearch['tracks']['items'][0]['name']}")
-        return songSearch['tracks']['items'][0]['id']
+        songSearch = sp.search(q=f'artist: "{serialized.subtitle}" track: "{serialized.title}"', type="track", market=mkt, limit=1)
+        if len(songSearch['tracks']['items']) != 0:
+            print(f"Song in Spotify: {songSearch['tracks']['items'][0]['artists'][0]['name']} - {songSearch['tracks']['items'][0]['name']}")
+            return songSearch['tracks']['items'][0]['id']
+        else:
+            print("Song was recognized, but wasn't found. Skipping... (try searching it manually, maybe the Shazam title is different from the one in Spotify)")
+            return "1"
     else:
         print("Couldn't recognize song. Skipping...")
         return "0"
@@ -66,12 +71,14 @@ playlistID = getPlaylistID(sp.current_user_playlists(limit=50))
 for dir, subdir, files in os.walk(SongDir):
     for f in files:
         trackID = asyncio.run(getTrackID(f"{dir}\{f}"))
-        if trackID != "0":
+        if trackID == "0":
+            moveFile(dir, f, True)
+        elif trackID == "1":
+            moveFile(dir, f, True, True)
+        else:
             songs.append(trackID)
             moveFile(dir, f)
-        else:
-            moveFile(dir, f, True)
-
+            
 print("Adding songs to playlist...")
 sp.playlist_add_items(playlistID, songs)
 print("Songs added!")
