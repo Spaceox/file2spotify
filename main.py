247ad29f10c1461cf16f3a4910a7f588bfe03bd1
Spaceox <91277246+Spaceox@users.noreply.github.com>
2022-09-24 23:44:45 +0200

    Way better song detection in Spotify

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 from rich.progress import Progress, SpinnerColumn, TimeElapsedColumn
 from spotipy.oauth2 import SpotifyOAuth
-from shazamio import Shazam, Serialize
+from shazamio import Shazam
 from pathlib import Path
 import spotipy
 import asyncio
@@ -16,11 +16,19 @@ SuccessDir = "done"
 IgnoredDir = "ignored"
 UnknownDir = f"{IgnoredDir}/unknown"
 NotFoundDir = f"{IgnoredDir}/not_found"
-mkt = "IT" # Select your Spotify market (or leave it blank)
+AlwaysUseAlternativeSearch = False  # Always use artist + name instead of isrc
+mkt = "IT"  # Select your Spotify market (or leave it blank)
 
 # But not these ones
-scope = 'playlist-read-private, playlist-modify-private'
-sp = spotipy.Spotify(auth_manager=SpotifyOAuth(scope=scope, client_id=ClientID, client_secret=ClientSecret, redirect_uri="http://localhost:8888/callback"))
+scope = "playlist-read-private, playlist-modify-private"
+sp = spotipy.Spotify(
+    auth_manager=SpotifyOAuth(
+        scope=scope,
+        client_id=ClientID,
+        client_secret=ClientSecret,
+        redirect_uri="http://localhost:8888/callback",
+    )
+)
 sh = Shazam()
 songs = []
 
@@ -30,18 +38,24 @@ progressbar = Progress(
     TimeElapsedColumn(),
 )
 
+
 def getPlaylistID(playlists: list) -> string:
-    for item in playlists['items']:
-        if item['name'] == PlaylistName:
-            progressbar.console.print(f"Found playlist {PlaylistName}")
-            return item['id']
+    for item in playlists["items"]:
+        if item["name"] == PlaylistName:
+            progressbar.console.log(f"Found playlist {PlaylistName}")
+            return item["id"]
 
-    userID = sp.me()['id']
-    playlist = sp.user_playlist_create(userID, PlaylistName, public=False, collaborative=False)
-    progressbar.console.print(f"Created playlist {PlaylistName}")
-    return playlist['id']
+    userID = sp.me()["id"]
+    playlist = sp.user_playlist_create(
+        userID, PlaylistName, public=False, collaborative=False
+    )
+    progressbar.console.log(f"Created playlist {PlaylistName}")
+    return playlist["id"]
 
-def moveFile(dir: string, file: string, ignored: bool = False, recognized: bool = False) -> None:
+
+def moveFile(
+    dir: string, file: string, ignored: bool = False, recognized: bool = False
+) -> None:
     if ignored:
         if recognized:
             os.rename(f"{dir}{file}", f"{NotFoundDir}/{file}")
@@ -50,32 +64,62 @@ def moveFile(dir: string, file: string, ignored: bool = False, recognized: bool 
     else:
         os.rename(f"{dir}/{file}", f"{SuccessDir}/{file}")
 
+
 async def getTrackID(songFile: string) -> string:
-    progressbar.console.print(f"Processing file: {songFile}")
+    progressbar.console.log(f"Processing file: {songFile}")
     out = await sh.recognize_song(songFile)
-    if len(out['matches']) != 0:
-        serialized = Serialize.track(data=out["track"])
-        progressbar.console.print(f"Recognized song: {serialized.subtitle} - {serialized.title}")
-        songSearch = sp.search(q=f'artist: "{serialized.subtitle}" track: "{serialized.title}"', type="track", market=mkt, limit=1)
-        if len(songSearch['tracks']['items']) != 0:
-            progressbar.console.print(f"Song in Spotify: {songSearch['tracks']['items'][0]['artists'][0]['name']} - {songSearch['tracks']['items'][0]['name']}")
-            return songSearch['tracks']['items'][0]['id']
+    if len(out["matches"]) != 0:
+        progressbar.console.log(
+            f"Recognized song: {out['track']['subtitle']} - {out['track']['title']}"
+        )
+        
+        if "isrc" in out["track"]:
+            songSearch = sp.search(
+                q=f'isrc: "{out["track"]["isrc"]}"', type="track", market=mkt, limit=1
+            )
+
+        try:
+            sSearchItems = len(songSearch["tracks"]["items"])
+        except UnboundLocalError:
+            sSearchItems = 0
+
+        if (
+            AlwaysUseAlternativeSearch
+            or sSearchItems == 0
+            or "isrc" not in out["track"]
+        ):
+            progressbar.console.log("Using alternative search")
+            songSearch = sp.search(
+                q=f'artist: "{out["track"]["subtitle"]}" track: "{out["track"]["title"]}"',
+                type="track",
+                market=mkt,
+                limit=1,
+            )
+
+        if len(songSearch["tracks"]["items"]) != 0:
+            progressbar.console.log(
+                f"Song in Spotify: {songSearch['tracks']['items'][0]['artists'][0]['name']} - {songSearch['tracks']['items'][0]['name']}"
+            )
+            return songSearch["tracks"]["items"][0]["id"]
         else:
-            progressbar.console.print("Song was recognized, but wasn't found. Skipping... (try searching it manually, maybe the Shazam title is different from the one in Spotify)")
+            progressbar.console.log(
+                "Song was recognized, but wasn't found in Spotify. Skipping..."
+            )
             return "1"
     else:
-        progressbar.console.print("Couldn't recognize song. Skipping...")
+        progressbar.console.log("Shazam couldn't recognize song. Skipping...")
         return "0"
 
+
 if not os.path.isdir(SongDir):
-    progressbar.console.print(f"{SongDir} doesn't exist. Exiting...")
+    progressbar.console.log(f"{SongDir} doesn't exist. Exiting...")
     exit(1)
 
 if len(os.listdir(SongDir)) == 0:
-    progressbar.console.print(f"{SongDir} is empty. Exiting...")
+    progressbar.console.log(f"{SongDir} is empty. Exiting...")
     exit(1)
 
-#Path(IgnoredDir).mkdir(parents=False, exist_ok=True)
+# Path(IgnoredDir).mkdir(parents=False, exist_ok=True)
 Path(UnknownDir).mkdir(parents=True, exist_ok=True)
 Path(NotFoundDir).mkdir(parents=False, exist_ok=True)
 Path(SuccessDir).mkdir(parents=False, exist_ok=True)
@@ -89,14 +133,26 @@ with progressbar as prog:
     for f in paths:
         trackID = asyncio.run(getTrackID(f))
         if trackID == "0":
-            moveFile(f.replace("\\", "/")[: f.rfind("/")], f.replace("\\", "/")[f.rfind("/") :], True)
+            moveFile(
+                f.replace("\\", "/")[: f.rfind("/")],
+                f.replace("\\", "/")[f.rfind("/") :],
+                True,
+            )
         elif trackID == "1":
-            moveFile(f.replace("\\", "/")[: f.rfind("/")], f.replace("\\", "/")[f.rfind("/") :], True, True)
+            moveFile(
+                f.replace("\\", "/")[: f.rfind("/")],
+                f.replace("\\", "/")[f.rfind("/") :],
+                True,
+                True,
+            )
         else:
             songs.append(trackID)
-            moveFile(f.replace("\\", "/")[: f.rfind("/")], f.replace("\\", "/")[f.rfind("/") :])
+            moveFile(
+                f.replace("\\", "/")[: f.rfind("/")],
+                f.replace("\\", "/")[f.rfind("/") :],
+            )
         prog.update(process, advance=1)
 
-progressbar.console.print("Adding songs to playlist...")
+progressbar.console.log("Adding songs to playlist...")
 sp.playlist_add_items(playlistID, songs)
-progressbar.console.print("Songs added!")
+progressbar.console.log("Songs added!")
